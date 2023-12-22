81b75e10bc4cdd47171c81afc666fe80c54c4695
frankfullstack <frankfullstack@gmail.com>
2023-12-22 21:42:28 +0100

    Modulo-02 -Ejercicios entregables -  Init and last

--- modulo-02/src/02-entregables/ejercicio-01.ts
+++ modulo-02/src/02-entregables/ejercicio-01.ts
@@ -77,7 +77,7 @@ type Tail<T> = T extends [unknown, ...infer Tail] ? Tail : never;
 
 const tailTypeResult:Tail<TailArrayValues> = [5, 18, 23, 57]; // You can try updating this value
 
-console.log(`Result of headTypeResult constant from Head type: ${tailTypeResult}`);
+console.log(`Result of tailTypeResult constant from Head type: ${tailTypeResult}`);
 
 // Typed function (b)
 
@@ -86,3 +86,68 @@ function tailTypedFunction<T>(values: T[]): T[]{
     return last;
 }
 console.log(`Result of Typed function: ${tailTypedFunction(INPUT_TAIL_VALUES)}`);
+
+/*****************
+ * Init Function *
+ *****************/
+
+// Example input values
+const INPUT_INIT_VALUES = [10, 5, 18, 23, 57];
+
+
+// Function declaration
+function init(values: number[]) {
+    return values.slice(0, -1);
+}
+
+console.log(`Input values: ${INPUT_INIT_VALUES}`)
+console.log(`Result of init function declaration: ${init(INPUT_INIT_VALUES)}` );
+
+// Function expression
+const init2 = (values: number[]) => {
+    return values.slice(0, -1);
+}
+
+console.log(`Result of init2 function expression: ${init2(INPUT_INIT_VALUES)}` );
+
+// Typed function
+
+function initTypedFunction<T>(values: T[]): T[]{
+    return values.slice(0, -1);
+}
+console.log(`Result of Typed function: ${initTypedFunction(INPUT_INIT_VALUES)}`);
+
+/*****************
+ * Last Function *
+ *****************/
+
+// Example input values
+const INPUT_LAST_VALUES = [10, 5, 18, 23, 57];
+
+
+// Function declaration (a)
+function last(values: number[]) {
+    // Options:
+    // or return values.slice(-1).pop(),
+    // or return values.at(-1);
+    return values[values.length-1];
+}
+
+
+console.log(`Input values: ${INPUT_LAST_VALUES}`)
+console.log(`Result of last function declaration: ${last(INPUT_LAST_VALUES)}` );
+
+// Function expression
+const last2 = (values: number[]) => {
+    return values[values.length-1];
+}
+
+console.log(`Result of last2 function expression: ${last2(INPUT_LAST_VALUES)}` );
+
+// Typed function
+
+function lastTypedFunction<T>(values: T[]): T{
+    return values[values.length-1];
+}
+console.log(`Result of Typed function: ${lastTypedFunction(INPUT_LAST_VALUES)}`);
+
--- modulo-02/transpiled/02-entregables/ejercicio-01.js
+++ modulo-02/transpiled/02-entregables/ejercicio-01.js
@@ -44,11 +44,56 @@ var tail2 = function (values) {
 };
 console.log("Result of tail2 function expression: ".concat(tail2(INPUT_TAIL_VALUES)));
 var tailTypeResult = [5, 18, 23, 57]; // You can try updating this value
-console.log("Result of headTypeResult constant from Head type: ".concat(tailTypeResult));
+console.log("Result of tailTypeResult constant from Head type: ".concat(tailTypeResult));
 // Typed function (b)
 function tailTypedFunction(values) {
     var last = values.slice(1);
     return last;
 }
 console.log("Result of Typed function: ".concat(tailTypedFunction(INPUT_TAIL_VALUES)));
+/*****************
+ * Init Function *
+ *****************/
+// Example input values
+var INPUT_INIT_VALUES = [10, 5, 18, 23, 57];
+// Function declaration
+function init(values) {
+    return values.slice(0, -1);
+}
+console.log("Input values: ".concat(INPUT_INIT_VALUES));
+console.log("Result of init function declaration: ".concat(init(INPUT_INIT_VALUES)));
+// Function expression
+var init2 = function (values) {
+    return values.slice(0, -1);
+};
+console.log("Result of init2 function expression: ".concat(init2(INPUT_INIT_VALUES)));
+// Typed function
+function initTypedFunction(values) {
+    return values.slice(0, -1);
+}
+console.log("Result of Typed function: ".concat(initTypedFunction(INPUT_INIT_VALUES)));
+/*****************
+ * Last Function *
+ *****************/
+// Example input values
+var INPUT_LAST_VALUES = [10, 5, 18, 23, 57];
+// Function declaration (a)
+function last(values) {
+    // Options:
+    // or return values.slice(-1).pop(),
+    // or return values.at(-1);
+    return values[values.length - 1];
+}
+console.log("Input values: ".concat(INPUT_LAST_VALUES));
+console.log("Result of last function declaration: ".concat(last(INPUT_LAST_VALUES)));
+// Function expression
+var last2 = function (values) {
+    return values[values.length - 1];
+};
+console.log("Result of last2 function expression: ".concat(last2(INPUT_LAST_VALUES)));
+// Typed function
+function lastTypedFunction(values) {
+    return values[values.length - 1];
+}
+console.log("Result of Typed function: ".concat(lastTypedFunction(INPUT_LAST_VALUES)));
 export {};
