1847f1ab5a528003e93c2772c77a109968ef1a8d
frankfullstack <frankfullstack@gmail.com>
2023-12-25 19:03:40 +0100

    Modulo-02 -Ejercicios entregables -  Clone

--- modulo-02/src/02-entregables/ejercicio-03.ts
+++ modulo-02/src/02-entregables/ejercicio-03.ts
@@ -1,1 +1,46 @@
 console.log("************** DELIVERABLE 03 *********************");
+
+/*******************
+ * Clone Function *
+ *******************/
+
+interface User {
+    firstName: string;
+    lastName: string;
+    mail: string;
+}
+const USER_INPUT_FOR_CLONE: User = {
+    firstName: "Jonh",
+    lastName: "Doe",
+    mail: "jonh.doe@mail.com"
+};
+
+// Clone function declaration
+
+function clone<T>(source: T):T {
+    return { ...source };
+}
+
+console.log(clone<User>(USER_INPUT_FOR_CLONE));
+
+// Clone function expression
+
+const clone2 = <T>(source: T):T => {
+    return { ...source };
+}
+
+console.log(clone2<User>(USER_INPUT_FOR_CLONE));
+
+// Clone alternative (a)
+const clone3 = <T>(source: T):T => {
+    return Object.assign({}, source);
+}
+
+console.log(clone3(USER_INPUT_FOR_CLONE));
+
+// Clone alternative (b)
+const clone4 = <T>(source: T):T => {
+    return JSON.parse(JSON.stringify(source));
+}
+
+console.log(clone4(USER_INPUT_FOR_CLONE));
--- modulo-02/transpiled/02-entregables/ejercicio-03.js
+++ modulo-02/transpiled/02-entregables/ejercicio-03.js
@@ -1,1 +1,37 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 console.log("************** DELIVERABLE 03 *********************");
+var USER_INPUT_FOR_CLONE = {
+    firstName: "Jonh",
+    lastName: "Doe",
+    mail: "jonh.doe@mail.com"
+};
+// Clone function declaration
+function clone(source) {
+    return __assign({}, source);
+}
+console.log(clone(USER_INPUT_FOR_CLONE));
+// Clone function expression
+var clone2 = function (source) {
+    return __assign({}, source);
+};
+console.log(clone2(USER_INPUT_FOR_CLONE));
+// Clone alternative (a)
+var clone3 = function (source) {
+    return Object.assign({}, source);
+};
+console.log(clone3(USER_INPUT_FOR_CLONE));
+// Clone alternative (b)
+var clone4 = function (source) {
+    return JSON.parse(JSON.stringify(source));
+};
+console.log(clone4(USER_INPUT_FOR_CLONE));
